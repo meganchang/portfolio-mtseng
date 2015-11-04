37b99f21334db86b1e1d1ae6dff73e89697b322c
Megan Chang <megamegerr@gmail.com>
2015-11-03 20:18:39 -0800

    First pass at hosting on gh-pages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var jade = require('gulp-jade');
 
 gulp.task('build-sass', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css/'));
 });
+
+gulp.task('jade2html', function() {
+	gulp.src('views/**/*.jade')
+		.pipe(jade())
+		.pipe(gulp.dest('./dist/html/'));
+})
 
 //Watch task
 gulp.task('default',function() {
